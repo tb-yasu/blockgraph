1e2f56d28662c1b20fd60f1080988525371e80af
tb-yasu <jkl213lkj>
2014-12-21 08:31:50 +0900

    add BlockGraphUtil.hpp

---
+++ src/BlockGraphUtil.hpp
@@ -0,0 +1,24 @@
+/* 
+ *  Copyright (c) 2014 Yasuo Tabei
+ * 
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ * 
+ *   1. Redistributions of source code must retain the above Copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *   2. Redistributions in binary form must reproduce the above Copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *
+ *   3. Neither the name of the authors nor the names of its contributors
+ *      may be used to endorse or promote products derived from this
+ *      software without specific prior written permission.
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+const uint64_t PRIME = 101;
